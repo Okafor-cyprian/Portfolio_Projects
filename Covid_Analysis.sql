4b95e7fc8d024813a1daaa4a76d0a6e918804d1b
Cyprian <125751508+Okafor-cyprian@users.noreply.github.com>
2026-01-10 15:02:13 +0100

    Add files via upload

---
+++ Covid_Analysis.sql
@@ -0,0 +1,321 @@
+-- Covid 19 Data Exploration Project
+
+-- Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+-- Please Note I'm Using Postgre SQL
+
+-- Step 1 Create the Table
+CREATE TABLE covid_deaths (
+    iso_code VARCHAR(10),
+    continent VARCHAR(50),
+    location VARCHAR(100),
+    date DATE,
+
+    population BIGINT,
+
+    total_cases NUMERIC,
+    new_cases NUMERIC,
+    new_cases_smoothed NUMERIC,
+
+    total_deaths NUMERIC,
+    new_deaths NUMERIC,
+    new_deaths_smoothed NUMERIC,
+
+    total_cases_per_million NUMERIC,
+    new_cases_per_million NUMERIC,
+    new_cases_smoothed_per_million NUMERIC,
+
+    total_deaths_per_million NUMERIC,
+    new_deaths_per_million NUMERIC,
+    new_deaths_smoothed_per_million NUMERIC,
+
+    reproduction_rate NUMERIC,
+
+    icu_patients NUMERIC,
+    icu_patients_per_million NUMERIC,
+
+    hosp_patients NUMERIC,
+    hosp_patients_per_million NUMERIC,
+
+    weekly_icu_admissions NUMERIC,
+    weekly_icu_admissions_per_million NUMERIC,
+
+    weekly_hosp_admissions NUMERIC,
+    weekly_hosp_admissions_per_million NUMERIC
+);
+
+-- Step 2, Import the Data
+COPY covid_deaths (
+    iso_code,
+    continent,
+    location,
+    date,
+    population,
+    total_cases,
+    new_cases,
+    new_cases_smoothed,
+    total_deaths,
+    new_deaths,
+    new_deaths_smoothed,
+    total_cases_per_million,
+    new_cases_per_million,
+    new_cases_smoothed_per_million,
+    total_deaths_per_million,
+    new_deaths_per_million,
+    new_deaths_smoothed_per_million,
+    reproduction_rate,
+    icu_patients,
+    icu_patients_per_million,
+    hosp_patients,
+    hosp_patients_per_million,
+    weekly_icu_admissions,
+    weekly_icu_admissions_per_million,
+    weekly_hosp_admissions,
+    weekly_hosp_admissions_per_million
+)
+FROM 'C:/temp/CovidDeaths.csv'
+WITH (
+    FORMAT csv,
+    HEADER true,
+    DELIMITER ',',
+    NULL ''
+);
+
+-- Confirm the data was imported
+SELECT * FROM covid_deaths;
+
+-- Create the Second Table
+CREATE TABLE covid_vaccinations (
+    iso_code VARCHAR(10),
+    continent VARCHAR(50),
+    location VARCHAR(100),
+    date DATE,
+
+    population BIGINT,
+
+    total_cases NUMERIC,
+
+    new_tests NUMERIC,
+    total_tests NUMERIC,
+    total_tests_per_thousand NUMERIC,
+    new_tests_per_thousand NUMERIC,
+    new_tests_smoothed NUMERIC,
+    new_tests_smoothed_per_thousand NUMERIC,
+
+    positive_rate NUMERIC,
+    tests_per_case NUMERIC,
+    tests_units VARCHAR(50),
+
+    total_vaccinations NUMERIC,
+    people_vaccinated NUMERIC,
+    people_fully_vaccinated NUMERIC,
+    new_vaccinations NUMERIC,
+    new_vaccinations_smoothed NUMERIC,
+
+    total_vaccinations_per_hundred NUMERIC,
+    people_vaccinated_per_hundred NUMERIC,
+    people_fully_vaccinated_per_hundred NUMERIC,
+    new_vaccinations_smoothed_per_million NUMERIC,
+
+    stringency_index NUMERIC,
+
+    population_density NUMERIC,
+    median_age NUMERIC,
+    aged_65_older NUMERIC,
+    aged_70_older NUMERIC,
+    gdp_per_capita NUMERIC,
+    extreme_poverty NUMERIC,
+    cardiovasc_death_rate NUMERIC,
+    diabetes_prevalence NUMERIC,
+    female_smokers NUMERIC,
+    male_smokers NUMERIC,
+    handwashing_facilities NUMERIC,
+    hospital_beds_per_thousand NUMERIC,
+    life_expectancy NUMERIC,
+    human_development_index NUMERIC
+);
+
+-- Copy the data set
+COPY covid_vaccinations (
+    iso_code,
+    continent,
+    location,
+    date,
+    population,
+    total_cases,
+    new_tests,
+    total_tests,
+    total_tests_per_thousand,
+    new_tests_per_thousand,
+    new_tests_smoothed,
+    new_tests_smoothed_per_thousand,
+    positive_rate,
+    tests_per_case,
+    tests_units,
+    total_vaccinations,
+    people_vaccinated,
+    people_fully_vaccinated,
+    new_vaccinations,
+    new_vaccinations_smoothed,
+    total_vaccinations_per_hundred,
+    people_vaccinated_per_hundred,
+    people_fully_vaccinated_per_hundred,
+    new_vaccinations_smoothed_per_million,
+    stringency_index,
+    population_density,
+    median_age,
+    aged_65_older,
+    aged_70_older,
+    gdp_per_capita,
+    extreme_poverty,
+    cardiovasc_death_rate,
+    diabetes_prevalence,
+    female_smokers,
+    male_smokers,
+    handwashing_facilities,
+    hospital_beds_per_thousand,
+    life_expectancy,
+    human_development_index
+)
+FROM 'C:/temp/CovidVaccinations.csv'
+WITH (
+    FORMAT csv,
+    HEADER true,
+    DELIMITER ',',
+    NULL ''
+);
+
+-- Confirm the data was imported
+SELECT * FROM covid_vaccinations;
+
+-- Select Data that we want to start with
+SELECT LOCATION, date, total_cases, new_cases, total_deaths, population
+FROM covid_deaths
+WHERE continent IS NOT NULL 
+ORDER BY iso_code, continent;
+
+-- Shows what percentage of population infected with Covid in each country
+SELECT Location, date, Population, total_cases, (total_cases/population)*100 AS PercentPopulationInfected
+FROM covid_deaths
+ORDER BY iso_code, continent;
+
+-- Countries with Highest Infection Rate compared to their Population
+SELECT Location, Population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 AS PercentPopulationInfected
+FROM covid_deaths
+GROUP BY Location, Population
+ORDER BY PercentPopulationInfected DESC;
+
+-- Countries with Highest Death Count per Population
+SELECT Location, MAX(CAST(Total_deaths AS int)) AS TotalDeathCount
+FROM covid_deaths
+WHERE continent IS NOT NULL 
+GROUP BY Location
+ORDER BY TotalDeathCount DESC;
+
+-- Showing contintents with the highest death count per population
+SELECT continent, MAX(CAST(Total_deaths AS int)) AS TotalDeathCount
+FROM covid_deaths
+WHERE continent IS NOT NULL 
+GROUP BY continent
+ORDER BY TotalDeathCount DESC;
+
+-- Total Cases, deaths and death percentage
+SELECT 
+    SUM(new_cases) AS total_cases,
+    SUM(CAST(new_deaths AS int)) AS total_deaths,
+    SUM(CAST(new_deaths AS int)) / SUM(New_Cases) * 100 AS DeathPercentage
+FROM covid_deaths
+WHERE continent IS NOT NULL;
+
+-- Total Population compared with Total Vaccination
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+FROM covid_deaths dea
+JOIN covid_vaccinations vac
+ON dea.location = vac.location
+AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL;
+
+-- Total Population compared with Total Vaccination
+SELECT 
+    dea.continent,
+    dea.location,
+    dea.date,
+    dea.population,
+    vac.new_vaccinations,
+    SUM(vac.new_vaccinations::int) OVER (
+        PARTITION BY dea.location 
+        ORDER BY dea.date
+        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+    ) AS RollingPeopleVaccinated
+FROM covid_deaths dea
+JOIN covid_vaccinations vac
+ON dea.location = vac.location
+AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+ORDER BY dea.location, dea.date;
+
+-- USE CTE
+WITH popvsvacc (Continent, Location, Date, Population, RollingPeopleVaccinated) AS (
+    SELECT 
+        dea.continent,
+        dea.location,
+        dea.date,
+        dea.population,
+        SUM(vac.new_vaccinations::int) OVER (
+            PARTITION BY dea.location 
+            ORDER BY dea.date
+            ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+        ) AS RollingPeopleVaccinated
+    FROM covid_deaths dea
+    JOIN covid_vaccinations vac
+    ON dea.location = vac.location
+    AND dea.date = vac.date
+    WHERE dea.continent IS NOT NULL
+)
+SELECT *
+FROM popvsvacc
+ORDER BY Location, Date;
+
+-- Using Temp Table to perform Calculation on Partition By in previous query
+DROP TABLE IF EXISTS percentpopulationvaccinated;
+
+CREATE TEMP TABLE percentpopulationvaccinated (
+    continent VARCHAR(255),
+    location VARCHAR(255),
+    date DATE,
+    population NUMERIC,
+    new_vaccinations NUMERIC,
+    rollingpeoplevaccinated NUMERIC
+);
+
+INSERT INTO percentpopulationvaccinated
+SELECT dea.continent,
+       dea.location,
+       dea.date,
+       dea.population,
+       vac.new_vaccinations,
+       SUM(vac.new_vaccinations::integer)
+           OVER (PARTITION BY dea.location ORDER BY dea.date)
+FROM covid_deaths dea
+JOIN covid_vaccinations vac
+ON dea.location = vac.location
+AND dea.date = vac.date;
+
+SELECT *,
+       (rollingpeoplevaccinated / population) * 100 AS percentpopulationvaccinated
+FROM percentpopulationvaccinated;
+
+CREATE OR REPLACE VIEW percentpopulationvaccinated AS
+SELECT dea.continent,
+       dea.location,
+       dea.date,
+       dea.population,
+       vac.new_vaccinations,
+       SUM(vac.new_vaccinations::integer)
+           OVER (PARTITION BY dea.location ORDER BY dea.date)
+           AS rollingpeoplevaccinated
+FROM covid_deaths dea
+JOIN covid_vaccinations vac
+ON dea.location = vac.location
+AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL;
